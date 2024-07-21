fc08d9ba6139324a6f3061a383011c21208380cd
Rusho Binnabi <RushoBinnabi123@yahoo.com>
2024-07-21 17:54:10 -0400

    started work on setting up the interface

---
+++ ArcanicVosforator/src/data.py
@@ -0,0 +1,20 @@
+vosforRates = {3, 12, 14, 18, 20, 21, 22, 24, 28, 84, 98}
+
+arbitrationArcaneSet = {"Arcane Blade Charger", "Arcane Bodyguard",
+                        "Arcane Pistoleer", "Arcane Primary Charger",
+                        "Arcane Tanker"}
+
+mirrorDefenseArcaneSet = {"Arcane Double Back", "Arcane Steadfast"
+                          "Primary Plated Round", "Secondary Encumber",
+                          "Secondary Kinship"}
+
+caviaArcaneSet = {"Melee Fortification", "Melee Retaliation", "Melee Animosity",
+                  "Melee Exposure", "Melee Influence", "Melee Vortex", "Melee Crescendo",
+                  "Melee Duplicate"}
+
+plagueStarArcaneSet = {"Exodia Contagion", "Exodia Epidemic"}
+
+cetusArcaneSet = {"Exodia Brave", "Exodia Force", "Exodia Hunt", "Exodia Might",
+                  "Exodia Triumph", "Exodia Valor"}
+
+# continue here later with the Duviri Arcane Set.
