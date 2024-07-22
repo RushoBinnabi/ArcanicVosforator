ca809575e3aa89d02ebc511c0973b2d1d9673f88
Rusho Binnabi <RushoBinnabi123@yahoo.com>
2024-07-22 14:04:01 -0400

    continued work setting up the data.

--- ArcanicVosforator/src/data.py
+++ ArcanicVosforator/src/data.py
@@ -17,4 +17,30 @@ plagueStarArcaneSet = {"Exodia Contagion", "Exodia Epidemic"}
 cetusArcaneSet = {"Exodia Brave", "Exodia Force", "Exodia Hunt", "Exodia Might",
                   "Exodia Triumph", "Exodia Valor"}
 
-# continue here later with the Duviri Arcane Set.
+duviriArcaneSet = {"Akimbo Slip Shot", "Arcane Intention", "Arcane Power Ramp", "Magus Aggress",
+                   "Primary Blight", "Primary Exhilarate", "Primary Obstruct", "Secondary Outburst",
+                   "Shotgun Vendetta", "Arcane Reaper", "Longbow Sharpshot", "Secondary Shiver"}
+
+fortunaArcaneSet = {"Pax Bolt", "Pax Charge", "Pax Seeker", "Pax Soar"}
+
+luaConjunctionArcaneSet = {"Arcane Blessing", "Arcane Rise", "Conjunction Voltage", "Primary Frostbite"}
+
+necraliskArcaneSet = {"Residual Boils", "Residual Malodor", "Residual Shock", "Residual Viremia",
+                      "Theorem Contagion", "Theorem Demulcent", "Theorem Infection"}
+
+eidolonArcaneSet = {"Arcane Acceleration", "Arcane Aegis", "Arcane Agility", "Arcane Arachne",
+                    "Arcane Avenger", "Arcane Awakening", "Arcane Barrier", "Arcane Consequence",
+                    "Arcane Deflection", "Arcane Energize", "Arcane Eruption", "Arcane Fury",
+                    "Arcane Grace", "Arcane Guardian", "Arcane Healing", "Arcane Ice", "Arcane Momentum",
+                    "Arcane Nullifier", "Arcane Phantasm", "Arcane Precision", "Arcane Pulse", "Arcane Rage",
+                    "Arcane Resistance", "Arcane Strike", "Arcane Tempo,", "Arcane Trickery", "Arcane Ultimatum",
+                    "Arcane Velocity", "Arcane Victory", "Arcane Warmth"}
+
+
+steelPathArcaneSet = {"Primary Deadhead", "Primary Dexterity", "Primary Merciless", "Secondary Deadhead",
+                      "Secondary Dextirity", "Secondary Merciless.", "Secondary Dextirity", "Secondary Merciless"}
+quillsArcaneSet = {"Magus Vigor, ""Virtous Null", "Magus Husk", "Virtous Tempo", "Virtous Fury,", "Magus Cadence"
+                   "Magus Cloud", "Magus Elevant", "Magus Nourish", "Magus Replenish", "Virtuous Ghost",
+                   "Virtuous Shadow", "Virtous Strike"}
+
+# continue here with the Vox Soalris arcane set.
