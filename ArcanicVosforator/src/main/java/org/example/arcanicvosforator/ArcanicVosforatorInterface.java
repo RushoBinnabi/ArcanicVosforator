0b78ecfcf8900e72f29614cdd76068e7ae0881cc
Rusho Binnabi <RushoBinnabi123@yahoo.com>
2024-07-26 15:03:36 -0400

    restructured the code for the UI and started work on adding filtering options.

--- ArcanicVosforator/src/main/java/org/example/arcanicvosforator/ArcanicVosforatorInterface.java
+++ ArcanicVosforator/src/main/java/org/example/arcanicvosforator/ArcanicVosforatorInterface.java
@@ -15,6 +15,22 @@ import javafx.stage.Stage;
 public class ArcanicVosforatorInterface extends Application {
 
     private final ArcanicVosforatorData data = new ArcanicVosforatorData();
+    ComboBox arcaneSetList = new ComboBox();
+    TextField arcaneQuantity = new TextField();
+    HBox arcaneSetBreakdown = new HBox();
+    HBox buttons = new HBox();
+    Button selectArcane = new Button();
+    Button clearArcanes = new Button();
+    ScrollPane listOfArcanes = new ScrollPane();
+    Text label = new Text();
+    VBox arcaneBreakdown = new VBox();
+    GridPane grid = new GridPane();
+    Scene scene;
+    VBox arcaneSetFilters = new VBox();
+    CheckBox eidolonArcaneSetFilter = new CheckBox();
+    CheckBox cetusArcaneSetFilter = new CheckBox();
+    CheckBox arbitrationArcaneSetFilter = new CheckBox();
+    CheckBox steelPathArcaneSetFilter = new CheckBox();
 
     public ArcanicVosforatorData getData() {
         return data;
@@ -22,19 +38,19 @@ public class ArcanicVosforatorInterface extends Application {
 
     @Override
     public void start(Stage stage) {
-        getData().setEidolonArcaneSet();
-        ComboBox arcaneSetList = new ComboBox(FXCollections.observableArrayList(getData().getEidolonArcaneSet()));
-        TextField arcaneQuantity = new TextField();
-        HBox arcaneSetBreakdown = new HBox();
-        HBox buttons = new HBox();
-        Button selectArcane = new Button();
-        Button clearArcanes = new Button();
-        ScrollPane listOfArcanes = new ScrollPane();
+        eidolonArcaneSetFilter.setText("Eidolon Arcane Set");
+        cetusArcaneSetFilter.setText("Cetus Arcane Set");
+        arbitrationArcaneSetFilter.setText("Arbitration Arcane Set");
+        steelPathArcaneSetFilter.setText("Steel Path Arcane Set");
+        arcaneSetFilters.getChildren().addAll(eidolonArcaneSetFilter, cetusArcaneSetFilter, arbitrationArcaneSetFilter, steelPathArcaneSetFilter);
+        arcaneSetFilters.setPadding(new Insets(50,50,50,50));
+        arcaneSetFilters.setSpacing(12);
+        arcaneSetFilters.setAlignment(Pos.TOP_CENTER);
+        setArcanes();
+        getData().setVosforRates();
+        arcaneSetList.setItems(FXCollections.observableArrayList(getData().getEidolonArcaneSet()));
         listOfArcanes.setScaleX(2.2);
         listOfArcanes.setPrefSize(150, 450);
-        Text label = new Text();
-        VBox arcaneBreakdown = new VBox();
-        GridPane grid = new GridPane();
         buttons.getChildren().addAll(selectArcane, clearArcanes);
         buttons.setSpacing(12);
         buttons.setAlignment(Pos.CENTER);
@@ -42,20 +58,37 @@ public class ArcanicVosforatorInterface extends Application {
         arcaneBreakdown.setSpacing(12);
         selectArcane.setText("Select");
         clearArcanes.setText("Clear");
-        label.setText("Ember Heirloom Ass > Wisp Ass"); // placeholder text.
         label.setScaleX(1.4);
         label.setScaleY(1.4);
         arcaneSetBreakdown.getChildren().addAll(arcaneSetList, arcaneQuantity);
         arcaneSetBreakdown.setSpacing(12);
-        arcaneSetBreakdown.setPadding(new Insets(20, 20, 20, 20));
+        arcaneSetBreakdown.setPadding(new Insets(35, 35, 35, 35));
         grid.add(arcaneSetBreakdown, 0, 0);
+        grid.add(arcaneSetFilters, 0, 1);
         grid.add(arcaneBreakdown, 1, 1);
         grid.setHgap(120);
         grid.setVgap(-45);
-        Scene scene = new Scene(grid, 800, 600);
+        scene = new Scene(grid, 800, 620);
         stage.setScene(scene);
         stage.setTitle("Arcanic Vosforator");
         stage.show();
+    }
+
+    public void setArcanes() {
+        getData().setEidolonArcaneSet();
+        getData().setZarimanArcaneSet();
+        getData().setArbitrationArcaneSet();
+        getData().setQuillsArcaneSet();
+        getData().setNecraliskArcaneSet();
+        getData().setDuviriArcaneSet();
+        getData().setCaviaArcaneSet();
+        getData().setLuaConjunctionArcaneSet();
+        getData().setFortunaArcaneSet();
+        getData().setMirrorDefenseArcaneSet();
+        getData().setCetusArcaneSet();
+        getData().setPlagueStarArcaneSet();
+        getData().setSteelPathArcaneSet();
+        getData().setVoxSolarisArcaneSet();
     }
 
     public static void main(String[] args) {
